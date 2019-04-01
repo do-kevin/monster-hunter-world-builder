27c8e29736e622a518796093c9c6b2c515cf7a11
Kevin Do <do.kevin.vo@gmail.com>
2019-04-01 16:42:23 -0700

    Styles Login modal and btn

--- src/LoginSignup/LoginModal.js
+++ src/LoginSignup/LoginModal.js
@@ -13,7 +13,14 @@ class LoginModal extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.createUser}>Register Email</button>
+        <main className="loginContainer">
+          <h1>Sign Up</h1>
+          <button 
+            onClick={this.createUser}
+            className="loginContainer__registerEmail-btn">
+            Register Email
+          </button>
+        </main>
       </div>
     )
   }
