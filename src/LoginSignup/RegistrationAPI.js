2fdf3497ece7e0312d64aba8baed783d0d7b5c27
Kevin Do <do.kevin.vo@gmail.com>
2019-04-02 04:13:04 -0700

    Refactor code into try-catch block

--- src/LoginSignup/RegistrationAPI.js
+++ src/LoginSignup/RegistrationAPI.js
@@ -1,20 +1,14 @@
 import axios from 'axios';
 
 export async function registerEmail(userEmail) {
-	if (typeof userEmail !== 'string') console.warn('userEmail parameter is not a string.');
-	await axios
-		.post(`${process.env.REACT_APP_API_SERVER_URL}/users/`, {
+  if (typeof userEmail !== 'string') console.warn('userEmail parameter is not a string.');
+
+  try {
+    const response = await axios.post(`${process.env.REACT_APP_API_SERVER_URL}/users/`, {
 			email: userEmail
-		})
-		.then((response) => {
-			const { status, statusText, data } = response;
-			if (status === 201) {
-				console.log(statusText);
-				console.log(data.id);
-			}
-			console.log(response);
-		})
-		.catch((error) => {
-			console.error(error);
-    });
+    })
+    return response;
+  } catch (error) {
+    console.log('Error in RegistrationAPI:', error)
+  }
 }
