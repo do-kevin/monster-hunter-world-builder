429bee3aa6dba14eea4f574d5239dc6b3ffbd2ff
Kevin Do <do.kevin.vo@gmail.com>
2019-04-01 19:19:59 -0700

    Init Landing page and 404 page

---
+++ src/Misc/LandingPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import LoginModal from '../LoginSignup/LoginModal';
+
+export default class LandingPage extends Component {
+  render() {
+    return <LoginModal />;
+  }
+}
