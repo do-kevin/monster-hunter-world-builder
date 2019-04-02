2ec5527cd402f6432ddbbbd08abf5f14ea1fbe82
Kevin Do <do.kevin.vo@gmail.com>
2019-04-01 19:21:01 -0700

    Sets up catch all route
    Cleaner export
    
    Removes setState, already handled by Formik
    
    Adds query-string dependency
    
    Adds additional forms
    
    Styles input err messages
    
    Init VerificationScreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import LoginModal from './LoginSignup/LoginModal';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import LandingPage from './Misc/LandingPage';
+import NotFound from './Misc/NotFound';
+import VerificationScreen from './LoginSignup/VerificationScreen';
 
 class App extends Component {
+
   render() {
     return (
-      <div>
-        <LoginModal />
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path='/' component={LandingPage}/>
+            <Route exact path='/account_verification' component={VerificationScreen}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/LoginSignup/LoginModal.js
+++ src/LoginSignup/LoginModal.js
@@ -3,37 +3,59 @@ import { registerEmail } from './RegistrationAPI';
 import { Formik } from 'formik';
 
 class LoginModal extends Component {
-	state = {
-		email: ''
-	};
-
-	createUser = () => {
-		return registerEmail(this.state.email);
-	};
-
 	render() {
 		return (
 			<div>
 				<main className="loginContainer">
 					<h1>Sign Up</h1>
 					<Formik
-						initialValues={{ email: '' }}
-						onSubmit={(values) => {
-							this.setState(() => ({
-								email: values.email
-							}));
-							return registerEmail(this.state.email);
+						initialValues={{ email: '', firstName: '', lastName: '' }}
+						onSubmit={(values) => registerEmail(values.email)}
+						validate={({ firstName, lastName, email }) => {
+							let errors = {};
+							if (!firstName) {
+								errors.firstName = <span className='loginContainer__form__errorMessage'>{' '}required</span>;
+							} else if (!lastName) {
+								errors.lastName = <span className='loginContainer__form__errorMessage'>{' '}required</span>;
+							} else if (!email) {
+								errors.email = <span className='loginContainer__form__errorMessage'>{' '}required</span>;
+							} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
+								errors.email = <span className='loginContainer__form__errorMessage'>{' '}invalid email</span>;
+							}
+							return errors;
 						}}
-					>
-						{({ values, handleSubmit, handleChange, isSubmitting }) => (
+						render={({ values, handleSubmit, handleChange, isSubmitting, errors }) => (
 							<form className="loginContainer__form" onSubmit={handleSubmit}>
-								<input
-									type="email"
-									name="email"
-									onChange={handleChange}
-									value={values.email}
-									className="loginContainer__form__input"
-								/>
+								<div style={{ textAlign: 'left' }}>
+									<label>First name {errors.firstName}</label>
+									<input
+										type="text"
+										name="firstName"
+										onChange={handleChange}
+										value={values.firstName}
+										className="loginContainer__form__input"
+									/>
+								</div>
+								<div style={{ textAlign: 'left' }}>
+									<label>Last name {errors.lastName}</label>
+									<input
+										type="text"
+										name="lastName"
+										onChange={handleChange}
+										value={values.lastName}
+										className="loginContainer__form__input"
+									/>
+								</div>
+								<div style={{ textAlign: 'left' }}>
+									<label>Email {errors.email}</label>
+									<input
+										type="email"
+										name="email"
+										onChange={handleChange}
+										value={values.email}
+										className="loginContainer__form__input"
+									/>
+								</div>
 								<button
 									type="submit"
 									disabled={isSubmitting}
@@ -43,7 +65,7 @@ class LoginModal extends Component {
 								</button>
 							</form>
 						)}
-					</Formik>
+					/>
 				</main>
 			</div>
 		);
