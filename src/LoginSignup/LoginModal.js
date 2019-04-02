5ab56873753b9abe86cd6610a478d915ff05ad84
Kevin Do <do.kevin.vo@gmail.com>
2019-04-01 18:05:15 -0700

    Implements simple Formik

--- src/LoginSignup/LoginModal.js
+++ src/LoginSignup/LoginModal.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 import { registerEmail } from './RegistrationAPI';
+import { Formik } from 'formik';
 
 class LoginModal extends Component {
-  state = {
-    email: ''
-  }
+	state = {
+		email: ''
+	};
 
-  createUser = () => {
-    return registerEmail(this.state.email);
-  }
+	createUser = () => {
+		return registerEmail(this.state.email);
+	};
 
-  render() {
-    return (
-      <div>
-        <main className="loginContainer">
-          <h1>Sign Up</h1>
-          <button 
-            onClick={this.createUser}
-            className="loginContainer__registerEmail-btn">
-            Register Email
-          </button>
-        </main>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<main className="loginContainer">
+					<h1>Sign Up</h1>
+					<Formik
+						initialValues={{ email: '' }}
+						onSubmit={(values) => {
+							this.setState(() => ({
+								email: values.email
+							}));
+							return registerEmail(this.state.email);
+						}}
+					>
+						{({ values, handleSubmit, handleChange, isSubmitting }) => (
+							<form className="loginContainer__form" onSubmit={handleSubmit}>
+								<input
+									type="email"
+									name="email"
+									onChange={handleChange}
+									value={values.email}
+									className="loginContainer__form__input"
+								/>
+								<button
+									type="submit"
+									disabled={isSubmitting}
+									className="loginContainer__registerEmail-btn"
+								>
+									Register Email
+								</button>
+							</form>
+						)}
+					</Formik>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default LoginModal;
