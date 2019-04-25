1034da054a23e5ff4d5e89731e58f45e896443d8
Kevin Do <do.kevin.vo@gmail.com>
2019-04-24 19:30:11 -0700

    Fixes paths

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "redux/state/rootReducer";
+import rootReducer from "redux/state/RootReducer";
 
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const configureStore = () => {
@@ -14,7 +14,7 @@ const configureStore = () => {
 
   if (process.env.NODE_ENV !== "production") {
     if (module.hot) {
-      module.hot.accept("redux/state/profile/Reducers", () => {
+      module.hot.accept("redux/state/RootReducer", () => {
         store.replaceReducer(rootReducer);
       });
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/no-named-as-default-member */
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import configureStore from "redux/store";
+import configureStore from "redux/Store";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "react-table/react-table.css";
